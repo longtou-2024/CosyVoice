c332d18bdcb1b9835819303a49660da375a2141b
longtou.2024 <longtou.2024@kakaoent.com>
2025-06-02 06:35:04 +0000

    test tts

---
+++ test/test.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('third_party/Matcha-TTS')
+sys.path.append('.') # for cosyvoice
+from cosyvoice.cli.cosyvoice import CosyVoice2
+from cosyvoice.utils.file_utils import load_wav
+import torchaudio
+
+
+
+cosyvoice = CosyVoice2('pretrained_models/CosyVoice2-0.5B', load_jit=False, load_trt=False, load_vllm=False, fp16=False)
+
+prompt_speech_16k = load_wav('./test/ke_kim/F-A3-D-005-0051.wav', 16000)
+for i, j in enumerate(cosyvoice.inference_zero_shot('안녕하십니까, 오늘 오전 날씨는 맑고 오후는 구름이 조금 끼겠습니다.', '아빠 말씀에 엄마가 막내이모를 향해 화를 버럭 냈습니다.', prompt_speech_16k, stream=False)):
+    torchaudio.save('zero_shot_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
