55b8aea830bb0abef2988895619bd6ed858a4d1c
longtou.2024 <longtou.2024@kakaoent.com>
2025-07-11 06:32:15 +0000

    revert train stream code

--- h100/run_h100.py
+++ h100/run_h100.py
@@ -14,12 +14,12 @@ from kfp import kubernetes
 from kfp.dsl import PipelineTask
 from kfp.kubernetes import common
 
-IMAGE_URL = "us-central1-docker.pkg.dev/prod-ai-project/tts/cosyvoice:v4.1"
+IMAGE_URL = "us-central1-docker.pkg.dev/prod-ai-project/tts/cosyvoice:v4.2"
 N_GPU = 4
 N_CPU = "40"
 MEM_SIZE = "400Gi"
 MOUNT_PATH = "/home/longtou.2024/mount"
-MODEL_DIR = f"{MOUNT_PATH}/longtou/h100/exp/cosyvoice/20250710"
+MODEL_DIR = f"{MOUNT_PATH}/longtou/h100/exp/cosyvoice/20250711"
 CONFIG = f"{MODEL_DIR}/cosyvoice2_lt.yaml"
 TB_DIR = f"{MODEL_DIR}/tensorboard"
 #CKPT = f"{MODEL_DIR}/torch_ddp/epoch_0_step_140000.pt"
@@ -27,7 +27,7 @@ TB_DIR = f"{MODEL_DIR}/tensorboard"
 SHELL_COMMAND = f''' \
 export CUDA_VISIBLE_DEVICES="0,1,2,3" \
 && . ../../../activate_python.sh \
-&& ./run.sh --stage 1 --stop_stage 1 --model_dir {MODEL_DIR} --tensorboard_dir {TB_DIR} --conf {CONFIG} --from_mount true --from_prod true --train_data "gs://literature commbooks literature_speaking_rate literature_tone commbooks_speaking_rate commbooks_tone azure"
+&& ./run.sh --stage 1 --stop_stage 1 --model_dir {MODEL_DIR} --tensorboard_dir {TB_DIR} --conf {CONFIG} --from_mount true --from_prod true --train_data "gs://literature commbooks literature_speaking_rate literature_tone commbooks_speaking_rate commbooks_tone"
 '''
 
 def add_pod_annotation(
--- test/test.py
+++ test/test.py
@@ -6,22 +6,28 @@ from cosyvoice.utils.file_utils import load_wav
 import torchaudio
 from pathlib import Path
 
-from cosyvoice.dataset.webdataset import PROMPT_TEMPLATE
+#from cosyvoice.dataset.webdataset import PROMPT_TEMPLATE
 
 llm_path="/home/longtou.2024/projects/CosyVoice/examples/aihub/cosyvoice2/exp/cosyvoice2/llm/torch_ddp/llm_avg.pt"
 cosyvoice = CosyVoice2('/home/longtou.2024/mount/longtou/saved/cosyvoice/pretrained_models/CosyVoice2-0.5B', load_jit=False, load_trt=False, load_vllm=False, fp16=False, llm_path=llm_path)
 
 outdir = "outdir"
 Path(outdir).mkdir(exist_ok=True)
-prompt_speech_16k = load_wav('./test/azure/azure-01.wav', 16000)
-#prompt_speech_16k = load_wav('./test/ke_kim/F-A3-D-005-0051.wav', 16000)
-spk_id="azure"
+#prompt_speech_16k = load_wav('./test/azure/azure-01.wav', 16000)
+prompt_speech_16k = load_wav('./test/ke_kim/F-A3-D-005-0051.wav', 16000)
+#spk_id="azure"
 
 fout_names = ["base.wav", "fast.wav", "slow.wav"]
 
-for i, j in enumerate(cosyvoice.inference_instruct2('안녕하세요, 여러분의 친구, 박의주입니다.', PROMPT_TEMPLATE(spk_id=spk_id, emotion="", pitch="", tone="", infer=True), prompt_speech_16k, stream=False, text_frontend=False)):
+for i, j in enumerate(cosyvoice.inference_zero_shot('안녕하십니까, 오늘 오전 날씨는 맑고 오후는 구름이 조금 끼겠습니다.', '아빠 말씀에 엄마가 막내이모를 향해 화를 버럭 냈습니다.', prompt_speech_16k, stream=False)):
     torchaudio.save(f"{outdir}/{fout_names[0]}", j['tts_speech'], cosyvoice.sample_rate)
-for i, j in enumerate(cosyvoice.inference_instruct2('<fast>안녕하세요</fast>, 여러분의 친구, 박의주입니다.', PROMPT_TEMPLATE(spk_id=spk_id, emotion="", pitch="", tone="", infer=True), prompt_speech_16k, stream=False, text_frontend=False)):
+for i, j in enumerate(cosyvoice.inference_zero_shot('<fast>안녕하십니까</fast>, 오늘 오전 날씨는 맑고 오후는 구름이 조금 끼겠습니다.', '아빠 말씀에 엄마가 막내이모를 향해 화를 버럭 냈습니다.', prompt_speech_16k, stream=False, text_frontend=False)):
     torchaudio.save(f"{outdir}/{fout_names[1]}", j['tts_speech'], cosyvoice.sample_rate)
-for i, j in enumerate(cosyvoice.inference_instruct2('<slow>안녕하세요</slow>, 여러분의 친구, 박의주입니다.', PROMPT_TEMPLATE(spk_id=spk_id, emotion="", pitch="", tone="", infer=True), prompt_speech_16k, stream=False, text_frontend=False)):
+for i, j in enumerate(cosyvoice.inference_zero_shot('<slow>안녕하십니까</slow>, 오늘 오전 날씨는 맑고 오후는 구름이 조금 끼겠습니다.', '아빠 말씀에 엄마가 막내이모를 향해 화를 버럭 냈습니다.', prompt_speech_16k, stream=False, text_frontend=False)):
     torchaudio.save(f"{outdir}/{fout_names[2]}", j['tts_speech'], cosyvoice.sample_rate)
+
+
+
+
+#for i, j in enumerate(cosyvoice.inference_instruct2('안녕하세요, 여러분의 친구, 박의주입니다.', PROMPT_TEMPLATE(spk_id=spk_id, emotion="", pitch="", tone="", infer=True), prompt_speech_16k, stream=False, text_frontend=False)):
+#    torchaudio.save(f"{outdir}/{fout_names[0]}", j['tts_speech'], cosyvoice.sample_rate)
