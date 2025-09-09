c48e95d66740c2f9ca390d48b6ba567566cbd6cd
longtou.2024 <longtou.2024@kakaoent.com>
2025-09-09 02:15:30 +0000

    test update

--- test/test_celeb_vllm.py
+++ test/test_celeb_vllm.py
@@ -10,51 +10,89 @@ from cosyvoice.utils.file_utils import load_wav
 import torchaudio
 from pathlib import Path
 
+
 celect_script = [
     {
-        #"tag": "happy 3",
-        "tag": "happy",
+        "tag": "기쁨",
         "text": '와~ 드디어 우리 게임 출시됐어! 오늘은 밤새 파티다!',
     },
     {
-        #"tag": "sad 2",
-        "tag": "sad",
+        "tag": "슬픔",
         "text": '그때 그렇게 말하지 말 걸... 아직도 마음에 남아.',
     },
     {
-        "tag": "anxious",
+        "tag": "걱정",
         "text": '혹시 답장 아직 안 온 거예요? 뭔가 문제가 있는 건 아닌지...',
     },
     {
-        #"tag": "angry 2",
-        "tag": "angry",
+        "tag": "분노",
         "text": '아니, 왜 자꾸 그 사람 얘기만 하는 거야?',
     },
     {
-        "tag": "neutral",
+        "tag": "무감정",
         "text": '그날 이후, 둘은 다시 연락하지 않았다.',
     },
     {
-        "tag": "embarrassed",
+        "tag": "당황",
         "text": '아, 그 얘긴 그냥... 농담이었어요. 진짜예요...',
     },
     {
-        #"tag": "sad 3",
-        "tag": "sad",
+        "tag": "슬픔",
         "text": '주변엔 늘 사람들이 있는데... 왜 이렇게 외로울까.',
     },
     {
-        #"tag": "angry 3",
-        "tag": "angry",
+        "tag": "분노",
         "text": '그만 좀 해! 말했잖아, 난 그런 거 관심 없다고!',
     },
     {
-        #"tag": "happy 1",
-        "tag": "happy",
+        "tag": "기쁨",
         "text": '비 온 뒤에 무지개가 떴다. 마음 한편이 따뜻해졌다.',
     },
     {
-        "tag": "anxious",
+        "tag": "걱정",
+        "text": '혹시... 내가 이상하게 보이진 않았죠? 다들 쳐다보는 것 같아서...',
+    },
+]
+
+celect_script = [
+    {
+        "tag": "노년",
+        "text": '와~ 드디어 우리 게임 출시됐어! 오늘은 밤새 파티다!',
+    },
+    {
+        "tag": "일반",
+        "text": '그때 그렇게 말하지 말 걸... 아직도 마음에 남아.',
+    },
+    {
+        "tag": "아동",
+        "text": '혹시 답장 아직 안 온 거예요? 뭔가 문제가 있는 건 아닌지...',
+    },
+    {
+        "tag": "노년 어두운",
+        "text": '아니, 왜 자꾸 그 사람 얘기만 하는 거야?',
+    },
+    {
+        "tag": "일반 어두운",
+        "text": '그날 이후, 둘은 다시 연락하지 않았다.',
+    },
+    {
+        "tag": "아동 어두운",
+        "text": '아, 그 얘긴 그냥... 농담이었어요. 진짜예요...',
+    },
+    {
+        "tag": "노년 밝은",
+        "text": '주변엔 늘 사람들이 있는데... 왜 이렇게 외로울까.',
+    },
+    {
+        "tag": "일반 밝은",
+        "text": '그만 좀 해! 말했잖아, 난 그런 거 관심 없다고!',
+    },
+    {
+        "tag": "아동 밝은",
+        "text": '비 온 뒤에 무지개가 떴다. 마음 한편이 따뜻해졌다.',
+    },
+    {
+        "tag": "아동 밝은",
         "text": '혹시... 내가 이상하게 보이진 않았죠? 다들 쳐다보는 것 같아서...',
     },
 ]
@@ -70,7 +108,7 @@ def wrap_tag(tag, text):
     return " ".join(tagged)
 
 #llm_path="/home/longtou.2024/projects/CosyVoice/examples/aihub/cosyvoice2/exp/cosyvoice2/llm/torch_ddp/llm_avg.pt"
-llm_path="/home/longtou.2024/mount/longtou/h100/exp/cosyvoice/20250905_2/torch_ddp/epoch_1_step_30000.pt"
+llm_path="/home/longtou.2024/mount/longtou/h100/exp/cosyvoice/20250908/torch_ddp/epoch_0_step_14000.pt"
 cosyvoice = CosyVoice2('/home/longtou.2024/mount/longtou/saved/cosyvoice/pretrained_models/CosyVoice2-0.5B', load_jit=False, load_trt=False, load_vllm=True, fp16=False, llm_path=llm_path)
 
 outdir = "outdir"
@@ -89,7 +127,8 @@ for line in open("test/kast_emo/text", 'r').readlines():
 
 
 for d_idx, spk_id in enumerate(prompt_spk_sent):
-    if spk_id != "pororo": continue
+    #if spk_id != "pororo": continue
+    if spk_id not in ("minsick", "oneyoung"): continue
     prompt_sent = prompt_spk_sent[spk_id]
     prompt_speech_16k = prompt_spk_speech_16k[spk_id]
     for t_idx, item in enumerate(celect_script):
